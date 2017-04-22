3f0d6a840fe21d0800070e1d92f92594e069858e
Ezequiel <eopnetto@gmail.com>
2017-04-22 12:57:45 -0300

    REST API to consult people and phones

--- src/AppBundle/Controller/PhonesController.php
+++ src/AppBundle/Controller/PhonesController.php
@@ -36,26 +36,55 @@ class PhonesController extends Controller {
 // "get_person_phones"   [GET] /people/{personid}/phones
 
     public function getPhoneAction($personid, $phoneid) {
-        
+        $person = $this->getDoctrine()
+                ->getRepository('AppBundle:Person')
+                ->find($personid);
+
+        if ($person) {
+            $phones = $person->getPhones();
+
+            if (!$phones) {
+                throw $this->createNotFoundException(
+                        'No phones found for personid ' . $personid
+                );
+            }
+
+            foreach ($phones as $value) {
+                if ($value->getPhoneid() == $phoneid)
+                    $phone = $value;
+            }
+
+            if (!isset($phone)) {
+                throw $this->createNotFoundException(
+                        "Person $personid has no phone found for id  $phoneid"
+                );
+            }
+        } else {
+            throw $this->createNotFoundException(
+                    'No person found for id ' . $personid
+            );
+        }
+
+        return $phone;
     }
 
     // "get_person_phone"    [GET] /people/{personid}/phones/{phoneid}
-
-    public function newPhonesAction($personid) {
-        
-    }
-
-// "new_person_phones"   [GET] /people/{personid}/phones/new
-
-    public function editPhoneAction($personid, $phoneid) {
-        
-    }
-
-// "edit_person_phone"   [GET] /people/{personid}/phones/{phoneid}/edit
-
-    public function removePhoneAction($personid, $phoneid) {
-        
-    }
-
-// "remove_person_phone" [GET] /people/{personid}/phones/{phoneid}/remove
+//=======================================================
+//    public function newPhonesAction($personid) {
+//        
+//    }
+//
+//// "new_person_phones"   [GET] /people/{personid}/phones/new
+//
+//    public function editPhoneAction($personid, $phoneid) {
+//        
+//    }
+//
+//// "edit_person_phone"   [GET] /people/{personid}/phones/{phoneid}/edit
+//
+//    public function removePhoneAction($personid, $phoneid) {
+//        
+//    }
+//
+//// "remove_person_phone" [GET] /people/{personid}/phones/{phoneid}/remove
 }
